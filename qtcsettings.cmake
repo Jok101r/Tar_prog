dda66bc40e201128270f8cc2b68ffc2bf19c0138
Aleksandr Akumyanskiy <aleksandrakumyanskiy@MacBook-Pro-Aleksandr.local>
2020-07-10 12:17:06 +0700

    Update 1.4

--- qtcsettings.cmake
+++
@@ -1,6 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_CXX_COMPILER" "/usr/bin/clang++" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "/usr/bin/gcc" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "/Users/aleksandrakumyanskiy/Qt/5.13.0/clang_64" CACHE "STRING" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/Users/aleksandrakumyanskiy/Qt/5.13.0/clang_64/bin/qmake" CACHE "STRING" "" FORCE)
